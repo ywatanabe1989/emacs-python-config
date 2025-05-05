d5d875d671a0838595c01f654a98d267da3479f0
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-05 11:11:05 +1000

    Refactor runners module

--- epc-runners.el
+++ epc-runners.el
@@ -1,6 +1,6 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 ;;; Author: ywatanabe
-;;; Timestamp: <2025-04-24 14:49:20>
+;;; Timestamp: <2025-05-01 19:07:14>
 ;;; File: /home/ywatanabe/.emacs.d/lisp/emacs-python-config/epc-runners.el
 
 ;;; Copyright (C) 2025 Yusuke Watanabe (ywatanabe@alumni.u-tokyo.ac.jp)
@@ -19,26 +19,76 @@
       (pim-fix-imports))
     (kill-new content)))
 
+(defun epc--runners-paste-lines-to-vterm ()
+  "Paste region (or buffer) to vterm, line by line."
+  (interactive)
+  (let* ((content (if (region-active-p)
+                      (buffer-substring-no-properties
+                       (region-beginning) (region-end))
+                    (buffer-substring-no-properties (point-min)
+                                                    (point-max))))
+         (lines (split-string content "\n" t)))
+    (dolist (line lines)
+      (vterm-send-string line)
+      (vterm-send-return))))
+
+;; (defun epc-runners-paste-to-repl ()
+;;   "Paste code into a Python REPL window without changing focus."
+;;   (interactive)
+;;   (let ((original-window (selected-window))
+;;         target-window)
+;;     (epc--runners-copy-region-or-buffer)
+;;     (setq target-window (epc-repl-find-window original-window))
+;;     (if target-window
+;;         (with-selected-window target-window
+;;           (cond
+;;            ((epc-repl-check-if-in-shell)
+;;             (epc-repl-switch-shell-to-ipython)
+;;             (term-send-raw-string "paste\C-m"))
+;;            ((epc-repl-check-if-in-ipython)
+;;             (term-send-raw-string "paste\C-m"))
+;;            ((epc-repl-check-if-in-embedded-ipython)
+;;             (term-send-raw-string "paste\C-m"))
+;;            ((epc-repl-check-if-in-ipdb)
+;;             (term-send-raw-string "from IPython import embed\C-m")
+;;             (term-send-raw-string "embed()\C-m")
+;;             (term-send-raw-string "paste\C-m")
+;;             (term-send-raw-string "exit\C-m"))))
+;;       (message "No suitable Python REPL window found"))))
+
 (defun epc-runners-paste-to-repl ()
   "Paste code into a Python REPL window without changing focus."
   (interactive)
   (let ((original-window (selected-window))
-        target-window)
+        (file-path (buffer-file-name))
+        (target-window))
     (epc--runners-copy-region-or-buffer)
     (setq target-window (epc-repl-find-window original-window))
+
     (if target-window
         (with-selected-window target-window
           (cond
            ((epc-repl-check-if-in-shell)
             (epc-repl-switch-shell-to-ipython)
+            (term-send-raw-string
+             (format "__file__ = \"%s\"\C-m" file-path))
             (term-send-raw-string "paste\C-m"))
+
            ((epc-repl-check-if-in-ipython)
+            (term-send-raw-string
+             (format "__file__ = \"%s\"\C-m" file-path))
             (term-send-raw-string "paste\C-m"))
+
            ((epc-repl-check-if-in-embedded-ipython)
+            (term-send-raw-string
+             (format "__file__ = \"%s\"\C-m" file-path))
             (term-send-raw-string "paste\C-m"))
+
            ((epc-repl-check-if-in-ipdb)
             (term-send-raw-string "from IPython import embed\C-m")
             (term-send-raw-string "embed()\C-m")
+            (term-send-raw-string
+             (format "__file__ = \"%s\"\C-m" file-path))
             (term-send-raw-string "paste\C-m")
             (term-send-raw-string "exit\C-m"))))
       (message "No suitable Python REPL window found"))))
@@ -46,28 +96,8 @@
 (defun epc-runners-paste-new-session ()
   "Paste code into new IPython session."
   (interactive)
-  (epc--runners-copy-region-or-buffer)
-  (other-window 1)
-  (let ((repl-type (epc-repl-check-interactive-status)))
-    (cond
-     ((string= repl-type "shell")
-      (epc-repl-switch-shell-to-ipython)
-      (epc-repl-switch-ipython-to-shell)
-      (epc-repl-switch-shell-to-ipython))
-     ((string= repl-type "ipython")
-      (epc-repl-switch-ipython-to-shell)
-      (epc-repl-switch-shell-to-ipython))
-     ((string= repl-type "embed")
-      (epc-repl-switch-embed-to-ipython)
-      (epc-repl-switch-ipython-to-shell)
-      (epc-repl-switch-shell-to-ipython))
-     ((string= repl-type "ipdb")
-      (epc-repl-switch-ipdb-to-ipython)
-      (epc-repl-switch-ipython-to-shell)
-      (epc-repl-switch-shell-to-ipython)))
-    (when repl-type
-      (term-send-raw-string "paste\C-m")))
-  (other-window -1))
+  (epc-runners-switch-to-shell)
+  (epc-runners-paste-to-repl))
 
 (defun epc-runners-run-script ()
   "Run Python script in interactive terminal."
